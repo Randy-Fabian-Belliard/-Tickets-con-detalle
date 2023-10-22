21697882fbe0a9eee61b867df3b0c506ed4a2870
Randy <randyfabianbelliard191407@gmail.com>
2023-10-22 16:31:44 -0400

    Initial commit

---
+++ Shared/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Registro_de_Tickets_con_detalle_de_Mensajes.Shared
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string? Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
